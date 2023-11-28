7d4a00de1a53882cc029a634c3ca80853292dabc
Solraz <joshtreestump@gmail.com>
2023-11-28 14:12:55 -0300

    CRUD: Aliases

--- api/rest/functions/users/crud.php
+++ api/rest/functions/users/crud.php
@@ -8,11 +8,15 @@ $table = str_replace("/", "", $app_state['router']['subdomain']);
 $function = $app_state['router']['function'] ?: "get";
 
 /**
+ * Normal CRUD functions with Aliases.
+ * 
  * @var match
  * 
  * @return string
  */
 match ($function) {
   "get", "select" => run_function("/database/", "select"),
-  "add", "insert" => run_function("/database/", "select"),
+  "add", "insert" => run_function("/database/", "insert"),
+  "del", "rem", "renove", "delete" => run_function("/database/", "delete"),
+  "change", "set", "edit" => run_function("/database/", "delete"),
 };
